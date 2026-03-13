0dc54f0201c08717a880dfa8009e3c7dcaa08b48
elabbas <elabbas@WINDOWS-S7US3HP>
2026-03-13 20:06:54 +0400

    frontend fix

--- public/script.js
+++ public/script.js
@@ -6,26 +6,18 @@ const slider = document.getElementById("slider")
 const loginTab = document.getElementById("loginTab")
 const signupTab = document.getElementById("signupTab")
 
-function showLogin(){
+  function showLogin() {
+    document.getElementById('loginForm').classList.remove('hidden');
+    document.getElementById('signupForm').classList.add('hidden');
+    document.getElementById('loginTab').classList.add('active');
+    document.getElementById('signupTab').classList.remove('active');
+    document.getElementById('tabRow').classList.remove('signup-active');
+  }
 
-signupForm.classList.add("hidden")
-loginForm.classList.remove("hidden")
-
-slider.style.left="0%"
-
-loginTab.classList.add("active")
-signupTab.classList.remove("active")
-
-}
-
-function showSignup(){
-
-loginForm.classList.add("hidden")
-signupForm.classList.remove("hidden")
-
-slider.style.left="50%"
-
-signupTab.classList.add("active")
-loginTab.classList.remove("active")
-
-}
+  function showSignup() {
+    document.getElementById('signupForm').classList.remove('hidden');
+    document.getElementById('loginForm').classList.add('hidden');
+    document.getElementById('signupTab').classList.add('active');
+    document.getElementById('loginTab').classList.remove('active');
+    document.getElementById('tabRow').classList.add('signup-active');
+  }
